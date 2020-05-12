6cbe5a2bdc7c592482115a075e537ca6cbaa8413
Latosha Tims <ltims080913@hotmail.com>
2020-05-12 16:32:51 -0700

    created get and post operations. finished MVP

--- cars/carsRouter.js
+++ cars/carsRouter.js
@@ -3,3 +3,33 @@ const router = express.Router()
  
 const knex = require('knex')
 const db = require('../data/dbConnect')
+
+router.get('/', (req, res) => {
+    db('cars')
+    .then(cars => {
+        res.json(cars)
+    })
+    .catch(err => {
+        res.status(500).json({message: 'Failed to retrieve cars'})
+    })
+})
+
+router.post('/', (req, res) => {
+    const addCar = req.body
+    db('cars')
+    .insert(addCar)
+    .then(ids => {
+        db('cars')
+        .where({id: ids[0]})
+        .then(newCar => {
+            res.status(201).json(newCar)
+        })
+    })
+    .catch(err => {
+        console.log('POST error', err)
+        res.status(500).json({message: 'Failed to store car'})
+    })
+
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const server = express()
+const carsRouter = require('./cars/carsRouter')
 
 server.use(express.json())
+server.use('/api/car-dealer', carsRouter)
 
 module.exports = server
