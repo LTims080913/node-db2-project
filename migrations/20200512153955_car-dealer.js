db5b9a1f7525c4605ae33ee61875e37ce3aa85c5
Latosha Tims <ltims080913@hotmail.com>
2020-05-12 16:11:03 -0700

    set up schema

---
+++ migrations/20200512153955_car-dealer.js
@@ -0,0 +1,19 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('cars', tbl => {
+      tbl.increments()
+      tbl.string('vin', 128).notNullable().unique()
+      tbl.string('make', 128).notNullable()
+      tbl.string('model', 128).notNullable()
+      tbl.integer('mileage').notNullable()
+      tbl.string('transmission_type', 128)
+      tbl.string('status_of_title', 128)
+
+  } )
+
+
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('cars')
+};
